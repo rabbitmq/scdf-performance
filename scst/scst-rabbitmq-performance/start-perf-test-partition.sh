b5ac0afd0adef04ae7ce064486a2ac510ac3ac46
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-12-08 10:39:39 +0100

    Add URI to PerfTest command

--- scst/scst-rabbitmq-performance/start-perf-test-partition.sh
+++ scst/scst-rabbitmq-performance/start-perf-test-partition.sh
@@ -7,5 +7,6 @@ source .env
 for (( i=0; i<$PARTITIONS; i++ ))
 do
     nohup ${PERF_TEST_HOME}/bin/runjava com.rabbitmq.perf.PerfTest \
+        --uri amqp://$RABBITMQ_USERNAME:$RABBITMQ_PASSWORD@$RABBITMQ_HOST:$RABBITMQ_PORT/$RABBITMQ_VHOST \
         -x 1 -y 0 --predeclared --size 1000 -exchange "scst.partition.$i" -routing-key "$i" &
 done
