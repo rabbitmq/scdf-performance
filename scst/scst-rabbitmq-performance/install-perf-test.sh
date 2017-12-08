41e95a464481a9218a084154357e586601ad19e4
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-12-08 09:21:54 +0100

    Add PerfTest install script

---
+++ scst/scst-rabbitmq-performance/install-perf-test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#set -ex
+
+wget https://dl.bintray.com/rabbitmq/java-tools-milestones/perf-test/2.0.0.RC4/rabbitmq-perf-test-2.0.0.RC4-bin.tar.gz
+tar xzf rabbitmq-perf-test-2.0.0.RC4-bin.tar.gz
