cf9ea271cbef98b88f639a0babd47e24b7b2858f
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2017-12-14 15:25:32 +0100

    Skip GPG signing when building PerfTest

--- scst/scst-rabbitmq-performance/install-perf-test.sh
+++ scst/scst-rabbitmq-performance/install-perf-test.sh
@@ -9,6 +9,6 @@ git clone https://github.com/rabbitmq/rabbitmq-perf-test.git
 cd rabbitmq-perf-test
 git checkout ${PERF_TEST_TAG}
 cd ..
-./mvnw -f ./rabbitmq-perf-test/pom.xml clean package -P assemblies -DskipTests
+./mvnw -f ./rabbitmq-perf-test/pom.xml clean package -P assemblies -DskipTests -Dgpg.skip
 
 tar xzf rabbitmq-perf-test/target/rabbitmq-perf-test-${PERF_TEST_VERSION}-bin.tar.gz -C rabbitmq-perf-test/target
